b6e4b07a9b733c8c2c1cfa13fc909f662afa78e7
Matthew Fritz <mattf@burbankparanormal.com>
2022-01-15 19:41:01 -0800

    Implemented the metadata creation and source file extraction logic

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Extract Source Map Code
 
-A small NodeJS utility to extract code from a source map and attempts to re-create the structure of the original files.
+A small NodeJS tool to extract code from a source map and re-create the structure of the original files.
 
 This is just a small tool for my own purposes to make viewing the original code from a source map easier.
+
+## Usage
+
+```
+node extract.js sourceFilePath1.map sourceFilePath2.map sourceFilePath3.map
+```
+
+The tool can accept one or more command-line arguments, each representing the path to a source map file.
---
+++ extract.js
@@ -0,0 +1,78 @@
+const fs = require('node-fs-extra');
+const path = require('path');
+
+const sourcePathArray = process.argv.slice(2); // don't process "node" or the script name
+console.log("[INFO] Processing " + sourcePathArray.length + " source map file(s)...");
+console.log("");
+
+// iterate over the array of command-line parameters representing file paths
+sourcePathArray.forEach(function(sourceMapPath, sourceIndex) {
+	let dirName = "extracted_" + path.basename(sourceMapPath);
+	let metadataFileName = "metadata_" + path.basename(sourceMapPath);
+	let metadataFilePath = dirName + "/" + metadataFileName;
+
+	console.log("[INFO] Loading source map " + (sourceIndex+1) + ": " + sourceMapPath);
+	console.log("");
+	console.log("[INFO] Output directory " + (sourceIndex+1) + ": " + dirName);
+	console.log("[INFO] Metadata output file " + (sourceIndex+1) + ": " + metadataFilePath);
+	console.log("");
+
+	try {
+		// load the source map file as a JSON object
+		let sourceMapObj = fs.readJsonSync(sourceMapPath);
+		console.log("[INFO] Source map " + (sourceIndex+1) + " loaded. Parsing...");
+		console.log("");
+
+		// iterate over the sources, re-work file paths, and remove tokens
+		let sourceFilePaths = sourceMapObj.sources;
+		sourceFilePaths = sourceFilePaths.map(function(path) {
+			while(path.indexOf('../') != -1) {
+				path = path.replace('../', '');
+			}
+			return path;
+		}).filter(function(path) {
+			// we only want true source file paths
+			return path.indexOf('external ') == -1;
+		});
+
+		try {
+			let metadataObj = {
+				version: sourceMapObj.version,
+				names: sourceMapObj.names,
+				sources: sourceFilePaths,
+			};
+
+			// make the directory and then write the extracted metadata
+			console.log("[INFO] Creating metadata file...");
+			fs.mkdirsSync(dirName);
+			fs.writeJsonSync(metadataFilePath, metadataObj, {spaces: 3});
+			console.log("[INFO] Metadata file created");
+			console.log("");
+
+			// iterate over the source code mappings and extract
+			console.log("[INFO] Extracting " + sourceFilePaths.length + " source file(s) from source map " + (sourceIndex+1) + "...");
+			console.log("");
+			sourceFilePaths.forEach(function(path, index) {
+				try {
+					// write the matching indexed content from the sourceContent object
+					console.log("[INFO] Extracting source file " + (index+1) + ": " + path);
+					let fileContent = sourceMapObj.sourcesContent[index];
+					fs.outputFileSync(dirName + "/" + path, fileContent);
+					console.log("[INFO] Extracted source file");
+				} catch (e) {
+					console.log("[ERROR] Could not write output file: " + path);
+					console.log(e);
+				}
+			});
+			console.log("");
+			console.log("[INFO] Finished extracting " + sourceFilePaths.length + " file(s) from source map " + (sourceIndex+1));
+		} catch (e) {
+			console.log("[ERROR] Could not write metadata file: ", e);
+		}
+	} catch(e) {
+		console.log("[ERROR] Could not load source map " + (sourceIndex+1) + ": ", e);
+	}
+	console.log("");
+});
+
+console.log("[INFO] Done.");
